beef9e5452f6727666e31a340628abd05bc802d9
Mykhailo <mishased12@gmail.com>
2024-04-24 00:31:25 +0200

    update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  entry: {
+    index: './src/index.js',
+    app: './src/scripts/app.js',
+    taskClass: './src/scripts/classes/Task.js',
+    projectClass: './src/scripts/classes/Project.js',
+    handlers: './src/scripts/modules/Handlers.js',
+    layout: './src/scripts/modules/Layout.js',
+    UserStorage: './src/scripts/modules/UserStorage.js',
+    burger: './src/scripts/views/BurgerView.js',
+    filters: './src/scripts/views/FiltersView.js',
+    form: './src/scripts/views/FormView.js',
+    projects: './src/scripts/views/ProjectsView.js',
+    task: './src/scripts/views/TaskView.js',
+  },
   entry: './src/index.js',
   output: {
     filename: 'bundle.main.js',
@@ -24,6 +38,16 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    }
     ],
   },  
   devtool: 'inline-source-map',
